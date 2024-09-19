3c482c258aa15fa9baf18b8b1b0905061defb4d5
CounselJS <okpabicounsel@gmail.com>
2024-09-19 15:39:23 +0100

    Fixed logo

--- components/shared/Brand.tsx
+++ components/shared/Brand.tsx
@@ -7,7 +7,7 @@ const Brand: FC<BrandProps> = ({}) => {
   return (
     <div className="text-3xl font-bold ">
       <Link href={"/"}>
-        <Image className="h-[10rem] w-[10rem]" alt="LOgo" width={200} height={200} src={"/images/logo.png"} />
+        <Image className="h-[10rem] w-[10rem]" alt="LOgo" width={200} height={200} src={"/images/logo-nobg.png"} />
       </Link>
     </div>
   );
